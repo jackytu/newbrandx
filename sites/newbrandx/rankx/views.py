f46d576131e477a805b3ba62e13644b659a538ad
jackytu <zhongying.tu@gmail.com>
2015-10-24 09:53:07 +0800

    1st version

---
+++ sites/newbrandx/rankx/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def rankx(request):
+        return HttpResponse("Hello, world. You're at the polls index.")
+
+# Create your views here.
