9d606961dbeae3fccbce85c0d1651c31537c29b9
jackytu <zhongying.tu@gmail.com>
2015-10-25 21:29:28 +0800

    fix rank table

--- sites/newbrandx/rankx/views.py
+++ sites/newbrandx/rankx/views.py
@@ -27,43 +27,6 @@ def milk_chart_view(request):
         data = json.loads(data_file.read())
     return HttpResponse(json.dumps(data), content_type='application/json')
 
-'''
-def weather_chart_view(request):
-    #Step 1: Create a DataPool with the data we want to retrieve.
-    weatherdata = \
-        DataPool(
-           series=
-            [{'options': {
-               'source': MonthlyWeatherByCity.objects.all()},
-              'terms': [
-                'month',
-                'houston_temp',
-                'boston_temp']}
-             ])
-
-    #Step 2: Create the Chart object
-    cht = Chart(
-            datasource = weatherdata,
-            series_options =
-              [{'options':{
-                  'type': 'line',
-                  'stacking': False},
-                'terms':{
-                  'month': [
-                    'boston_temp',
-                    'houston_temp']
-                  }}],
-            chart_options =
-              {'title': {
-                   'text': 'Weather Data of Boston and Houston'},
-               'xAxis': {
-                    'title': {
-                       'text': 'Month number'}}})
-
-    #Step 3: Send the chart object to the template.
-    return render_to_response({'weatherchart': cht})
-
-'''
 def cloth(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
--- sites/newbrandx/templates/rankx/reports/scripts/generate_milk_report.py
+++ sites/newbrandx/templates/rankx/reports/scripts/generate_milk_report.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import time
 import MySQLdb
 import datetime
 
@@ -12,7 +12,7 @@ report_tittle = "milk/milk_{}.html".format(today.strftime("%Y_%m"))
 first = today.replace(day=1)
 last_year = first - datetime.timedelta(days=365)
 rang_low = last_year.strftime("%Y-%m-01")
-rang_high = today.strftime("%Y-%m-28")
+rang_high = today.strftime("%Y-%m-01")
 month_high = today.strftime("%Y_%m")
 month_low = last_year.strftime("%Y_%m")
 conn= MySQLdb.connect(
@@ -24,12 +24,26 @@ conn= MySQLdb.connect(
         )
 cur = conn.cursor()
 
-query = ["select rankx_brand.name, rankx_milk.index, rankx_milk.pub_date from rankx_milk",
+query = ["select",
+         "rankx_brand.name,",
+         "rankx_milk.brand_name_id,",
+         "rankx_milk.rank,",
+         "rankx_milk.pv,",
+         "rankx_milk.taobao_sales,",
+         "rankx_milk.jd_sales,",
+         "rankx_milk.tmall_sales,",
+         "rankx_milk.vip_sales,",
+         "rankx_milk.amazon_sales,",
+         "rankx_milk.weibo_fans,",
+         "rankx_milk.weibo_forward,",
+         "rankx_milk.weixin_fans,",
+         "rankx_milk.pub_date",
+         "from rankx_milk",
          "left join rankx_brand",
          "on rankx_milk.brand_name_id = rankx_brand.id",
-         "where rankx_milk.pub_date >",
+         "where rankx_milk.pub_date =",
          "'{}'".format(rang_low),
-         "and rankx_milk.pub_date <",
+         "or rankx_milk.pub_date =",
          "'{}'".format(rang_high),
          ]
 
@@ -38,12 +52,11 @@ sql_query = " ".join(query)
 results = cur.execute(sql_query)
 
 info = cur.fetchmany(results)
-cur.close()
-conn.commit()
-conn.close()
 
 for ii in info:
-    (brand_raw, index, pub_date) = ii
+    (brand_raw, brand_id, rank_raw, pv, taobao_sales, jd_sales,
+        tmall_sales, vip_sales, amazon_sales,
+        weibo_fans, weibo_forward, weixin_fans, pub_date) = ii
     brand = brand_raw.replace(' ', '')
     if not brand_data.has_key(brand):
         brand_data[brand] = {}
@@ -51,14 +64,28 @@ for ii in info:
     year = pub_date.year
     this_year = datetime.date.today().year
 
+    rank = (pv * 0.2 + (taobao_sales + jd_sales + tmall_sales + vip_sales + amazon_sales)*0.6 + (weibo_fans + weibo_forward + weixin_fans)*0.2)
+
     if year == this_year:
-        brand_data[brand]['this_year'] = index
+        brand_data[brand]['this_year'] = rank
     else:
-        brand_data[brand]['last_year'] = index
+        brand_data[brand]['last_year'] = rank
+
+    sql = "update rankx_milk set rank = {} where brand_name_id = {} and pub_date = '{}'".format(rank, brand_id, pub_date)
+    cur.execute(sql)
+    time.sleep(2)
+
+print brand_data
+
+cur.close()
+conn.commit()
+conn.close()
 
 for (brand, data) in brand_data.items():
-    if data['this_year'] >= data['last_year']:
+    if data['this_year'] > data['last_year']:
         data['trend'] = 'up'
+    elif data['this_year'] == data['last_year']:
+        data['trend'] = '-'
     else:
         data['trend'] = 'down'
 
@@ -68,6 +95,15 @@ for (brand, data) in brand_data.items():
         rating = 100
     data['rating'] = rating
 
+sort1 = sorted(brand_data.iteritems(), key=lambda d:d[1]['last_year'], reverse = False)
+brand_lyear = {}
+idx = 1
+for brand, data in sort1:
+    brand_lyear[brand] = idx
+    idx += 1
+
+sort2 = sorted(brand_data.iteritems(), key=lambda d:d[1]['this_year'], reverse = False)
+
 report = open(report_tittle, 'w')
 content = '''
 <div class = "row">
@@ -75,10 +111,12 @@ content = '''
 '''
 content += '<tr><td>{}</td><td>{}</td><td>趋势</td><td>品牌</td><td>得分</td><td>变化率</td></tr>'.format(month_high, month_low)
 
-for (brand, data) in brand_data.items():
+idx2 = 1
+for brand, data in sort2:
     content += '''
     <tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>90</td><td>{}</td></tr>
-    '''.format(data['this_year'], data['last_year'], data['trend'], brand, data['rating'])
+    '''.format(idx2, brand_lyear[brand], data['trend'], brand, data['rating'])
+    idx2 += 1
 content += '''
 </table>
 </div>
